27d8b9d9fc3e868c80b5def3fa14dadd529b30e6
ADMIN <ktushar4546@gmail.com>
2022-12-02 20:29:01 +0530

    code

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -7,16 +7,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button search,view;
+    RadioGroup src,dst;
+    RadioButton source,destination;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        src=findViewById(R.id.src);
+        dst=findViewById(R.id.dest);
+        search=findViewById(R.id.Submit);
+        view=findViewById(R.id.view);
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i1=src.getCheckedRadioButtonId();
+                int i2=dst.getCheckedRadioButtonId();
+                if(i1==i2)
+                {
+                    Toast.makeText(MainActivity.this, "Source and Destination cannot be same", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    source=findViewById(i1);
+                    destination=findViewById(i2);
 
+                }
+            }
+        });
 
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
 }
