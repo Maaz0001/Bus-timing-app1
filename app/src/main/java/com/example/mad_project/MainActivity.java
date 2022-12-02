b6a17c6df0cb0cb970add7aa25aa56e50b88f51a
Maaz0001 <maazabdul1306@gmail.com>
2022-12-02 20:15:30 +0530

    ok

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -9,26 +9,14 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button filter,view_all;
-    EditText b_src,b_dst,b_time;
-    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b_src=findViewById(R.id.bus_src);
-        b_dst=findViewById(R.id.bus_dst);
-        b_time=findViewById(R.id.bus_time);
-        filter=findViewById(R.id.Submit);
-        view_all=findViewById(R.id.view);
-        db = openOrCreateDatabase("BusTiming",MODE_PRIVATE,null);
 
-        filter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-            }
-        });
+
+
 
     }
 }
