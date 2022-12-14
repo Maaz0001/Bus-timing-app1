808b1d5119b8cdb26ae5bbef3f073784205f782d
ADMIN <ktushar4546@gmail.com>
2022-12-14 17:14:52 +0530

    SPlash Screen 12/14/2022

--- app/src/main/java/com/example/mad_project/MainActivity.java
+++ app/src/main/java/com/example/mad_project/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -123,5 +124,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
+
 }
